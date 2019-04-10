b0022585d1dce45b9aa2181a27760fdd44c71fcb
morp <rutesicm27@gmail.com>
2019-04-10 03:06:18 +0200

    Update #substring

--- project_03/substrings.rb
+++ project_03/substrings.rb
@@ -9,6 +9,28 @@ def substring(word, dictionary)
   return count
 end
 
+
+=begin
+  
+      ########### More concise way of this which i was to dumb to think of myself ##########
+
+def substrings(string, dictionary)
+
+  hash = {}
+
+  string.downcase! #We want all words to be lowercase so we can match them in the dictionary.
+
+  dictionary.each do |e|
+    if string.scan(/#{e}/).count > 0
+      hash[e] = string.scan(/#{e}/).count #Counts occurences of the regexp in the string and stores it in hash.
+    end
+  end
+
+  return hash
+end
+
+=end
+
 dictionary = ["below","down","go","going","horn","how","howdy","it",
               "i","low","own","part","partner","sit","howdy"]
 
