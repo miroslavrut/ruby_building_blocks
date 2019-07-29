6ef0d59f13a7f8431a33d1926e60737a1b4c7b1f
morp <rutesicm27@gmail.com>
2019-07-30 01:21:13 +0200

    Prepare for tests

---
+++ building_blocks/caesar_cipher/lib/caesar_cipher.rb
@@ -0,0 +1,23 @@
+def caesar_cipher(input, shift)
+  shift %= 26
+  letters = input.split("")
+  letters.map! do |letter|
+    letter_code = letter.ord + shift
+    if letter.match? /[a-z]/      
+      letter_code -= 26 if letter_code > 122
+      letter_code.chr
+    elsif letter.match? /[A-Z]/
+      letter_code -= 26 if letter_code > 90
+      letter_code.chr
+    else
+      letter
+    end
+  end
+  return letters.join("")
+end
+
+puts "String: "
+input = gets.chomp
+puts "Key: "
+key = gets.chomp.to_i
+puts caesar_cipher(input, key)
